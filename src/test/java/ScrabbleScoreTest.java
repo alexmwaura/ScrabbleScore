c48d4fd60a9ae4274405c95b43f83047d85b0009
alexmwaura <alexmwaura43@gmail.com>
2020-10-24 14:57:10 +0300

    add test cases

--- src/test/java/ScrabbleScoreTest.java
+++ src/test/java/ScrabbleScoreTest.java
@@ -4,9 +4,20 @@ import static org.junit.Assert.*;
 
 public class ScrabbleScoreTest {
     @Test
-    public void calculateScore_returnsScoreForSingleLetter_1() throws Exception {
+    public void calculateScore_returnsScoreForSingleLetter_1() {
         ScrabbleScore testScrabble = new ScrabbleScore();
         Integer expected = 1;
         assertEquals(expected, testScrabble.calculateScore("a"));
+    }
+    @Test
+    public void calculateScore_returnScoreForTwoLetter_4() {
+        ScrabbleScore testScrabble = new ScrabbleScore();
+        Integer expected = 4;
+        assertEquals(expected, testScrabble.calculateScore("ab"));
+    }
+    @Test public void calculateScore_returnScoreForThreeLetter_14(){
+        ScrabbleScore testScrabble = new ScrabbleScore();
+        Integer expected = 14;
+        assertEquals(expected, testScrabble.calculateScore("abz"));
     }
 }
