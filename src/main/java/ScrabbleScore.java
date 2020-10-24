88ed943256bdddd34f88205ffd7b1dc99b5ef52f
alexmwaura <alexmwaura43@gmail.com>
2020-10-24 14:56:43 +0300

    add for loop and switch case

--- src/main/java/ScrabbleScore.java
+++ src/main/java/ScrabbleScore.java
@@ -4,25 +4,41 @@ public class ScrabbleScore {
     public Integer calculateScore(String word){
         ArrayList<Integer> scrabbleScore = new ArrayList<Integer>();
         String[] firstCase = {"a", "e", "i", "o", "u", "l", "n", "r", "s", "t"};
+        String[] secondCase = {"d", "g"};
+        String[] thirdCase = {"b", "c", "m", "p"};
+        String[] fourthCase = {"f", "h", "v", "w", "y"};
         int count = 0;
-        for(char scrabble: word.toCharArray()){
+        for(char scrabble: word.toLowerCase().toCharArray()){
+            String scrabbleString = String.valueOf(scrabble);
             for(String fc: firstCase){
-                String scrabbleString = String.valueOf(scrabble);
-                if(fc.equals(scrabbleString)) {
-                    scrabbleScore.add(1);
-                }
+                if(fc.equals(scrabbleString))scrabbleScore.add(1);
+            }
+            for(String sc: secondCase){
+                if(sc.equals(scrabbleString)) scrabbleScore.add(2);
+            }
+            for(String thc: thirdCase){
+                if(thc.equals(scrabbleString)) scrabbleScore.add(3);
+            }
+            for(String fc: fourthCase){
+                if(fc.equals(scrabbleString)) scrabbleScore.add(4);
+            }
+            switch (scrabbleString){
+                case "k":
+                    scrabbleScore.add(5);
+                    break;
+                case "j":
+                case "x":
+                    scrabbleScore.add(8);
+                    break;
+                case "q":
+                case "z":
+                    scrabbleScore.add(10);
+                    break;
             }
         }
         for(int integerScore: scrabbleScore){
-            count =+ integerScore;
+            count =+ integerScore + count;
         }
         return count;
     }
 }
-//A, E, I, O, U, L, N, R, S, T       1
-//        D, G                               2
-//        B, C, M, P                         3
-//        F, H, V, W, Y                      4
-//        K                                  5
-//        J, X                               8
-//        Q, Z                               10
